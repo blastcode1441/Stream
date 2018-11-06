77071d923da8082556d4f128c2e46654917db6d0
blastcode1441 <44486185+blastcode1441@users.noreply.github.com>
2018-11-06 08:22:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,17 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const A7MD = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on("guildMemberAdd", member => {
-    member.createDM().then(function (channel) {
-        return channel.send(`:rose:  ولكم نورت السيرفر :rose: 
-        https://discord.gg/hfpFfDf  
-        انت العضو رقم ${member.guild.memberCount} `) 
-    }).catch(console.error)
+console.log("BOT ONLINE");
+ 
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+السلام وعليكم و رحمة الله بركاته
+ افضل سيرفر لبيع حسابات ماين كرافت و تاقات ديسكورد و كل شي رخيص مرا
+                             https://discord.gg/jhyNtsg
+اتمني انك تدخله ... [ ${member}  ]
+**`) 
+}).catch(console.error)
 })
-
-
-client.login(process.env.BOT_TOKEN);
+A7MD.login('التوكين حقك');
